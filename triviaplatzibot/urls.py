5494313b624e04b759411ed413e3b20d49094983
Renzo Tincopa <rtincopa@gmail.com>
2022-09-11 18:29:48 -0500

    se agrego un nuevo path par que herolu maneje statick files, segundo intento

--- triviaplatzibot/urls.py
+++ triviaplatzibot/urls.py
@@ -1,4 +1,4 @@
-#from ast import pattern
+from ast import pattern
 from django.contrib import admin
 from django.urls import path
 from .trivia.views import RandomQuestions
@@ -8,7 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/random/',RandomQuestions.as_view(), name='random'),
-    path(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    #path(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
 
-#urlpatterns += pattern('', (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
+urlpatterns += pattern('', (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
