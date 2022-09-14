c90bd5035c909ef2e79d075c9f4e4b849d7bb44a
Renzo Tincopa <67519194+TincoNomad@users.noreply.github.com>
2022-09-14 01:04:31 -0500

    Update urls.py
    Se modificó agregando el if

--- triviaplatzibot/urls.py
+++ triviaplatzibot/urls.py
@@ -8,7 +8,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/random/',RandomQuestions.as_view(), name='random'),
-    #path(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += pattern('', (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
