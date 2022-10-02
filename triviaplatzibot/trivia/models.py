1be96d34bd0c0e1855b3eced4564544df6336967
Renzo Tincopa <rtincopa@gmail.com>
2022-10-02 13:10:13 -0500

    agregando escuelas y URLs a las tablas

--- triviaplatzibot/trivia/models.py
+++ triviaplatzibot/trivia/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from random import choices
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -19,12 +21,13 @@ class Question(models.Model):
         (5,_('digital Content')),
         (6,_('startup')),
         (7,_('english')),
-
     )
 
     title = models.CharField(_('title'), max_length=250)
     points = models.SmallIntegerField(_('Points'))
     difficulty = models.IntegerField(_('Difficulty'), choices=LEVEL, default=0)
+    schools = models.IntegerField(_('Schools'), choices=SCHOOLS, default=0,)
+    url = models.URLField(_('url'), default='', null=True)
     is_active = models.BooleanField(_('Is Active'), default=True)
     created_at = models.DateTimeField(_('created'), auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(_('Updated'), auto_now=True, auto_now_add=False)
