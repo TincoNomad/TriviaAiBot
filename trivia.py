115c0cc5c9f3c024033bdeb4ab5b1fd5f3763848
Renzo Tincopa <rtincopa@gmail.com>
2022-12-19 10:38:48 -0500

    El juego ya funcione pero hay un bug al escoger varios opciones de cursos

--- trivia.py
+++ trivia.py
@@ -4,6 +4,7 @@ import requests
 import json
 
 
+###### Game data base get points ######
 def get_score():
     leaderboard = ''
     id = 1
@@ -18,12 +19,14 @@ def get_score():
 
     return(leaderboard)
 
+###### Game data base save points ######
 def update_score(name, points):
     #url = 'https://djangodiscordtriviabot.herokuapp.com/api/score'
     url = 'http://127.0.0.1:8000/api/score'
     new_score = {'name': name, 'points': points}
     x = requests.post(url, data=new_score)
 
+###### Game data base Courses ######
 def get_course(opcionEscuela, opcionNivel):
     course = ''
     numero = 1
@@ -33,81 +36,103 @@ def get_course(opcionEscuela, opcionNivel):
     for item in json_data:
         if item['school'] == int(opcionEscuela) and item['difficulty'] == int(opcionNivel):
             course += '\n' + str(numero) + '-' + item['title']
-        
+        else:
+            course += 'Ups, esto es vergonzoso, pero parece que aun no tenemos cursos con esas categorias 😅'
         item['title'] = numero
         numero += 1
+    
+    if course == 'Ups, esto es vergonzoso, pero parece que aun no tenemos cursos con esas categorias 😅':
+        numero = 0
 
     return(course,numero)
 
-def get_question(opcionCurso):
-
+###### Game data base Questions ######
+def get_question(opcionCurso,counter):
+    
+    print(opcionCurso)
     question = ''
     id = 1
     answer = ''
     points = 0
-    url = ''
     #response = requests.get('https://djangodiscordtriviabot.herokuapp.com/api/question/')
     response = requests.get('http://127.0.0.1:8000/api/question')
     json_data = json.loads(response.text)
     questionOptions = [i['question'] for i in json_data if i['title'] == opcionCurso]
-
-    for i in json_data:
-        if i['title'] == opcionCurso:
-            url = i['url']
-
-    question += questionOptions[0][0]['questionTitle'] + '\n\n'
-    for item in questionOptions[0][0]['answer']:
+        
+    question += questionOptions[0][counter]['questionTitle'] + '\n\n'
+    for item in questionOptions[0][counter]['answer']:
             question += str(id) + '-' + item['answerTitle'] + '\n'
 
             if item['is_correct']:
                 answer = id
             id += 1
     points = questionOptions[0][0]['points']
-    
-    return question, answer, points, url,
+
+    return question, answer, points
 
 
+#get course url
+def getLink(opcionCurso):
+    url = ''
+    #response = requests.get('https://djangodiscordtriviabot.herokuapp.com/api/question/')
+    response = requests.get('http://127.0.0.1:8000/api/question')
+    json_data = json.loads(response.text)
+    for i in json_data:
+        if i['title'] == opcionCurso:
+            url = i['url']
+    return url
+
+
+############Discord Bot#########
 class MyClient(discord.Client):
 
+#confirm bot conection
     async def on_ready(self):
         print('nos conectamos como', self.user)
 
+#sotp bot for looping
     async def on_message(self, message):
 
         if message.author == self.user:
             return
 
-        async def puntaje():
-            if message.content == '$puntaje':
-                leaderboard = get_score()
-                await message.channel.send(leaderboard)
+#save game's palyers score 
+        async def score():
+            leaderboard = get_score()
+            await message.channel.send(leaderboard)
 
+#get game's palyers score         
+        if message.content == '$puntaje':
+            await score()
+
+#game logic
         async def juego():
-                    
-            question, answer, points, url = get_question(opcionCurso)
+            
+            question, answer, points = get_question(opcionCurso,counter)
+
             await message.channel.send(question)
                 
             def check(message):
                 return message.author ==message.author and message.content.isdigit()
 
             try:
-                guess = await client.wait_for('message',check=check, timeout= 5.0)
+                guess = await client.wait_for('message',check=check, timeout= 1.0)
             except asyncio.TimeoutError:
-                return await message.channel.send('Ops, te demoraste mucho 🥲' + ', estas preguntas son del curso ' + url)
+                return await message.channel.send('Ops, te demoraste mucho 🥲')
+            await asyncio.sleep(1.0)
 
             if int(guess.content) == answer:
                 user = guess.author
-                mensaje = '¡Correcto! ' + str(guess.author.name) + ', ganaste ' + str(points) + ' puntos 🥳'
+                mensaje = '¡Correcto! ' + str(guess.author.name) + ', ganaste ' + str(points) + ' puntos 🥳' + '\n\n'
                 await message.channel.send(mensaje)
                 update_score(user, points)
-                #await message.channel.send(leaderboard)
-                await message.channel.send('estas preguntas son del curso ' + url)
-
             else:
-                await message.channel.send('Uy no, esa no es la respuesta 😞')
+                await message.channel.send('Uy no '+ str(guess.author.name) +', esa no es la respuesta 😞' + '\n\n')
 
+###### Begining of the game #######         
         if message.content == "$trivia":
 
+#school options
             escuelas = '''
 Escoje una escuela:
 1- developing
@@ -122,6 +147,8 @@ Escoje una escuela:
 10- english      
         
         '''
+
+#difficulty options
             dificultad = '''
 Escoje una dificulatad:
 1- Beginner
@@ -129,42 +156,99 @@ Escoje una dificulatad:
 3- Advance
         
         '''
-        
-            await message.channel.send('super!!! Comencemos!!!')
-            await message.channel.send(escuelas)
 
+#Welcome messages
+            mensaje1 = '''
+|￣￣￣￣￣￣￣￣￣￣￣|
+            ¡¡¡Game Time!!!  
+|＿＿＿＿＿＿＿＿＿＿＿| 
+                    \ (•◡•) / 
+                        |    |  
+                         ---
+                         |  |          
+            
+            '''
+            mensaje2 = '''
+```yaml
+-------------------
+   TRIVIA PLATZI  
+-------------------         
+```       
+            '''
+#begining of interaction
+            
+
+            await message.channel.send(mensaje1)
+            await message.channel.send(mensaje2)
+            await message.channel.send(''''Dentro de poco comenzará el juego, serna 5 preguntas yel tema sera uno de lo cursos de Platzi
+tendrán 10 segundos para escoger una respuesta''')
+            await message.channel.send('Comenzamos en 3 ⏳')
+            await message.author.send(escuelas)
+
+#recolect parameters
             def opciones(message):
-                #return message.author ==message.author and message.content.isdigit()
+
                 return message.content.isdigit()
             try:
                 respuesta = await client.wait_for('message',check=opciones)
             except asyncio:
-                return await message.channel.send('Ops, te demoraste mucho 🥲')
+                return
             opcionEscuela = respuesta.content
-            await message.channel.send(dificultad)
+            await message.channel.send('... 2 ⏳')
+            await message.author.send(dificultad)
             try:
                 respuesta = await client.wait_for('message',check=opciones)
             except asyncio:
-                return await message.channel.send('Ops, te demoraste mucho 🥲')
+                return 
             opcionNivel = respuesta.content
             get_course(opcionEscuela, opcionNivel)
-            curso, numero = get_course(opcionEscuela, opcionNivel)
-            await message.channel.send('Escoje un curso:')
-            await message.channel.send(curso)
-            try:
-                respuesta = await client.wait_for('message',check=opciones)
-            except asyncio:
-                return await message.channel.send('Ops, te demoraste mucho 🥲')
-            if respuesta.content == numero:
-                return curso
-            opcionCurso = curso[3:]
-            get_question(opcionCurso)
-            await juego()
-            
 
+#Choosing a course for the game
+            course, numero = get_course(opcionEscuela, opcionNivel)
+            while True:
+                if numero != 0:
+                    await message.channel.send('... 1 ⏳')
+                    await message.author.send('Escoje un curso:')
+                    await message.author.send(course)
+                    try:
+                        respuesta = await client.wait_for('message',check=opciones, timeout= 15.0)
+                    except asyncio.TimeoutError:
+                        return await message.author.send('''
+Ops, te demoraste mucho 😄, si aun no sabes sobre de cual curso hacer el juego no hay problema, revisa 
+la lista y luego vuelve a colcoar el comando $trivia''')
+                    print (course)
 
+                    if int(respuesta.content) + 1 == numero:
+                        #print(respuesta.content)
+                        cursoElegido = course
 
+                    elif int(respuesta.content) + 1 != numero:
+                        await message.author.send('Has escogido un opcion incorrecta, por favor intenta de nuevo 😊')
+                        continue
+                    opcionCurso = cursoElegido[3:]
+                    counter = 0
+                    get_question(opcionCurso,counter)
+                    getLink(opcionCurso)
+                    print('luego de escojer')
+                    print(course)
+                else:
+                        #await message.channel.send(curso)
+                    await message.author.send(course)
+                    break
 
+# game action.
+                counter = 0
+                while counter <= 4:
+                    await juego()
+                    counter += 1
+                await message.channel.send('Fin del juego, gracias por participar 💃🕺')
+                await message.channel.send('Fue muy divertido ¡Felicidades!' + '\n' + 'Puntaje final: ')
+                await score()
+                url = getLink(opcionCurso)
+                await message.channel.send('El tema de este juego fue el curso ' + url)
+                break
+
+##### Discord bot conection ######            
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
